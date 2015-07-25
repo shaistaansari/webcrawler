836541cc207d4463fab9013e906904e55aad6b22
shaista ansari <shaansari@hotmail.com>
2015-07-25 08:39:18 -0500

    second attemp at adding file to repo

---
+++ itunes.py
@@ -0,0 +1,40 @@
+
+import requests
+
+class AppCrawler:
+	def _init_(self, starting_url, depth):
+		self.starting_url = starting_url
+		self.depth = depth
+		self.apps = []
+
+def crawl(self):
+	return
+
+def get_app_from_link(self, link):
+	return
+
+class App:
+
+	def _init_(self, name, developer, price, links):
+		self.name = name
+		self.developer = developer
+		self.price = price
+		self.links = links
+
+def _str_(self):
+	return ("Name: " + self.name.enconde('UTF-8') +
+		"\r\rDeveloper: " + self.developer.encode('UTF-8') +
+		"\r\rPrice: " + self.price.encode('UTF-8') + "\r\n")
+
+
+crawler = AppCrawler('https://itunes.apple.com/us/app/candy-crush-saga/id553834731', 0)
+crawler.crawl()
+
+for app in crawler.apps:
+	print app
+
+
+
+
+
+	
